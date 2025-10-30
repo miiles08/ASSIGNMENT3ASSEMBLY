4ddece4c831bd1d4aeaad9e6d12718f9e4044db7
miiles08 <miles.hudock28@gmail.com>
2025-10-30 18:46:03 -0400

    Added Snapping Script test

---
+++ Assets/SnapComponent.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapPoint : MonoBehaviour
+{
+    public SnapType snapType; // Assign the correct part type in the Inspector
+}
